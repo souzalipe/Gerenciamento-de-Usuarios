3a34af1ad51938579801cd2a97d7ae2a08effd49
Felipe Souza <fn23886@gmail.com>
2024-09-16 23:05:28 -0300

    feat Creat , App , Home

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from "./Home-page/Home"
+import Home from "./Components/Home"
+import Create from "./Components/Create";
 import  "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -10,9 +11,8 @@ function App() {
   return (
     <BrowserRouter> 
       <Routes>
-        <Route path="/" element={<Home />}>
-
-        </Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/create" element={<Create />}></Route>
       </Routes>
     </BrowserRouter>
   )
--- frontEnd/src/Components/Home.jsx
+++ frontEnd/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
+import { Link } from "react-router-dom";
  
 function Home() {
     const [data,setData] = useState([])
@@ -10,15 +11,19 @@ function Home() {
     },[])
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center aling-itens-center">
-            <div className="w-50 bg-white rounded p-3">
+            <div className="w-50  h-50 bg-white rounded p-3">
                 <h2>Users List</h2>
+                <div className="d-flex justify-content-end">
+                    <Link to="/create" className="btn btn-success">Novo Usuário +</Link>
+                </div>
                 <table className="table">
                     <thead>
                         <tr>
-                            <th>id_user</th>
+                            <th>id</th>
                             <th>nome</th>
                             <th>telefone</th>
                             <th>email</th>
+                            <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
