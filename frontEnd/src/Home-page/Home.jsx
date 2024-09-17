976641b42e1dd806890812c056d779ae8634001e
Felipe Souza <fn23886@gmail.com>
2024-09-16 22:46:09 -0300

    feat estrutura

--- frontEnd/src/Home-page/Home.jsx
+++ frontEnd/src/Home-page/Home.jsx
@@ -9,7 +9,36 @@ function Home() {
         .catch(err => console.log(err));
     },[])
     return (
-        <></>
+        <div className="d-flex vh-100 bg-primary justify-content-center aling-itens-center">
+            <div className="w-50 bg-white rounded p-3">
+                <h2>Users List</h2>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>id_user</th>
+                            <th>nome</th>
+                            <th>telefone</th>
+                            <th>email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((users,index) =>{
+                            return <tr key={index}>
+                                <td>{users.id_user}</td>
+                                <td>{users.nome}</td>
+                                <td>{users.telefone}</td>
+                                <td>{users.email}</td>
+                                <td>
+                                    <button className="btn btn-sm btn-info">Ver</button>
+                                    <button className="btn btn-sm btn-primary mx-2">Editar</button>
+                                    <button className="btn btn-sm btn-danger">Deletar</button>
+                                </td>
+                            </tr>
+                        } )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
