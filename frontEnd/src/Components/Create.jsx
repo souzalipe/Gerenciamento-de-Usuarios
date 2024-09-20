5d804009f182e2a5ce8aad6d2390e4cb363db4f7
Felipe Souza <fn23886@gmail.com>
2024-09-19 22:05:46 -0300

    fix link

--- frontEnd/src/Components/Create.jsx
+++ frontEnd/src/Components/Create.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Create(){
     const [values, setValues] = useState({
